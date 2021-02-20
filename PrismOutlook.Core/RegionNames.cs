4adddec3203e24095b75b03dcd7e29008ef4c03b
rrdls <rramosdls.developer@gmail.com>
2021-02-20 18:49:31 -0300

    new branch

--- PrismOutlook.Core/RegionNames.cs
+++ PrismOutlook.Core/RegionNames.cs
@@ -2,7 +2,7 @@
 {
     public class RegionNames
     {
-
+        //test
         public static string ContentRegion = "ContentRegion";
         public static string OutlookGroupRegion = "OutlookGroupRegion";
         public static string RibbonRegion = "RibbonRegion";
